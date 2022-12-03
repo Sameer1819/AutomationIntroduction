bcb8090879e4b3fdd21f5a678a9ac445ee7de7c7
1819s <1819sameerag@gmail.com>
2022-12-04 00:03:21 +0530

    one line added

--- src/NewEmptyClass.java
+++ src/NewEmptyClass.java
@@ -3,7 +3,10 @@ public class NewEmptyClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		System.out.println("New");
+		System.out.println("New1");
+		
 	}
 
 }
